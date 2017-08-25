62de76504f84a4c5290e1d0a809efe0631b49414
staceynapier <staceynapier86@gmail.com>
2017-08-25 13:51:35 +0100

    created getPrice test for apple

--- app/src/test/java/com/example/user/shoppingbasket/AppleTest.java
+++ app/src/test/java/com/example/user/shoppingbasket/AppleTest.java
@@ -1,8 +1,25 @@
 package com.example.user.shoppingbasket;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by user on 25/08/2017.
  */
 
 public class AppleTest {
+
+    Apple apple;
+
+    @Before
+    public void before() {
+        apple = new Apple(0.30);
+    }
+
+    @Test
+    public void hasPrice(){
+        assertEquals(0.30, apple.getPrice());
+    }
 }
