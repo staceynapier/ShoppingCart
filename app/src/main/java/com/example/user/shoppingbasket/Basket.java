139bd4eb86c14d341deb3953aaea6a75ae8946a9
staceynapier <staceynapier86@gmail.com>
2017-08-25 14:42:45 +0100

    added @Before to initialize objects to add to basket and canAddToBasket test

--- app/src/main/java/com/example/user/shoppingbasket/Basket.java
+++ app/src/main/java/com/example/user/shoppingbasket/Basket.java
@@ -5,4 +5,6 @@ package com.example.user.shoppingbasket;
  */
 
 public class Basket {
+
+
 }
--- app/src/test/java/com/example/user/shoppingbasket/BasketTest.java
+++ app/src/test/java/com/example/user/shoppingbasket/BasketTest.java
@@ -1,8 +1,29 @@
 package com.example.user.shoppingbasket;
 
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by user on 25/08/2017.
  */
 
 public class BasketTest {
+
+    Basket basket;
+    Apple apple;
+    Banana banana;
+
+    @Before
+    public void before(){
+        basket = new Basket();
+        apple = new Apple(0.25, "Own Brand", "Braeburn");
+        banana = new Banana(0.45, "Chiquita");
+    }
+
+    @Test
+    public void canAddToBasket(){
+        basket.add(banana);
+
+    }
+
 }
