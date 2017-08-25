8e1d5b93f79142798a79ba2123000bf2033d5a73
staceynapier <staceynapier86@gmail.com>
2017-08-25 14:30:14 +0100

    created banana class and test - hasPrice test passing

---
+++ app/src/main/java/com/example/user/shoppingbasket/Banana.java
@@ -0,0 +1,11 @@
+package com.example.user.shoppingbasket;
+
+/**
+ * Created by user on 25/08/2017.
+ */
+
+public class Banana extends Item {
+    public Banana(Double price, String brand) {
+        super(price, brand);
+    }
+}
---
+++ app/src/test/java/com/example/user/shoppingbasket/BananaTest.java
@@ -0,0 +1,25 @@
+package com.example.user.shoppingbasket;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by user on 25/08/2017.
+ */
+
+public class BananaTest {
+
+    Banana banana;
+
+    @Before
+    public void before() {
+        banana = new Banana(0.50, "Del Monte");
+    }
+
+    @Test
+    public void hasPrice(){
+        assertEquals(0.50, banana.getPrice());
+    }
+}
